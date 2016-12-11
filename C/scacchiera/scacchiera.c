2f0c9ecc932793ef3d7422cfc0435362bcfb3f9c
Teodoro777 <teodoro.coluccio@gmail.com>
2016-12-11 20:57:14 +0100

    Risolto il problema con uno scambio di colori

--- C/scacchiera/scacchiera.c
+++ C/scacchiera/scacchiera.c
@@ -5,9 +5,11 @@
 #include <stdlib.h>
 #include <ncurses.h>
 
+int scambio_colore(int color);
+
 int main()
 {
-  int i = 0, righe_quadrati = 2, righe_scacchiera = 0, colonne_scacchiera = 10;
+  int i = 0, righe_quadrati = 2, righe_scacchiera = 0, colonne_scacchiera = 10, colore = 1;
 
   initscr();
   curs_set(0);
@@ -25,24 +27,31 @@ int main()
   
   while (colonne_scacchiera < 66) {
     righe_scacchiera = 0; /* Riporto al valore iniziale il numero delle righe */
-    righe_quadrati = 2;
+    righe_quadrati = 2; /* Riporto alla riga dove deve stampare */
+    
+    colore = scambio_colore(colore);
+    
     while (righe_scacchiera < 4) { /* Stampa 8 quadrati */
-  
+     
+      colore = scambio_colore(colore);
+
       /* Stampa primo quadrato */
-      attron(COLOR_PAIR(1));
+      attron(COLOR_PAIR(colore));
       for (i = 0; i < 4; i++) {
         mvaddstr(righe_quadrati, colonne_scacchiera, "       ");
         righe_quadrati++;
       }
-      attroff(COLOR_PAIR(1));
+      attroff(COLOR_PAIR(colore));
+    
+      colore = scambio_colore(colore);
       
       /* Stampa secondo quadrato */
-      attron(COLOR_PAIR(2));
+      attron(COLOR_PAIR(colore));
       for (i = 0; i < 4; i++) {
         mvaddstr(righe_quadrati, colonne_scacchiera, "       "); 
         righe_quadrati++;
       }
-      attroff(COLOR_PAIR(2));
+      attroff(COLOR_PAIR(colore));
 
       righe_scacchiera++; /* Aggiorna il numero di righe */
     }
@@ -55,4 +64,13 @@ int main()
 
   return 0;
   
+}
+
+/* Funzione per lo scambio di colore */
+int scambio_colore(int color)
+{
+  if (color == 1)
+    return 2;
+  else
+    return 1;
 }
