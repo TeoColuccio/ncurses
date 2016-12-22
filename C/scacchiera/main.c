b04aca5f52da1084ee6ee029ac9e274b2102fc82
Teodoro777 <teodoro.coluccio@gmail.com>
2016-12-22 20:05:29 +0100

    Creati due cicli per la stampa

--- C/scacchiera/main.c
+++ C/scacchiera/main.c
@@ -8,8 +8,8 @@
 
 int main()
 {
-  int riga_iniziale = 2, colonna_iniziale = 9, altezza_quadrato = 4, larghezza_quadrato = 4;
-
+  int riga_iniziale = 1, colonna_iniziale = 9, altezza_quadrato = 3, larghezza_quadrato = 4, temp = riga_iniziale, i = 0, c = 0;
+  
   /* Nella funzione ho dichiarato le coppie di colore:
    * - 1 = COLOR_BLACK, COLOR_WHITE
    * - 2 = COLOR_BLACK, COLOR_RED
@@ -18,14 +18,25 @@ int main()
    */
 
   draw_init();
-
-  draw_square(1, altezza_quadrato, larghezza_quadrato, colonna_iniziale, riga_iniziale);
-  draw_square(1, altezza_quadrato, larghezza_quadrato, colonna_iniziale + altezza_quadrato, riga_iniziale + larghezza_quadrato);
-draw_square(2, altezza_quadrato, larghezza_quadrato, colonna_iniziale
-    + larghezza_quadrato, riga_iniziale);
-draw_square(2, altezza_quadrato, larghezza_quadrato, colonna_iniziale, riga_iniziale + altezza_quadrato);
   
-draw_end();
+  while (i < 4) {
+    while (c < 4) {
+      draw_square(1, altezza_quadrato, larghezza_quadrato, colonna_iniziale, riga_iniziale);
+      draw_square(1, altezza_quadrato, larghezza_quadrato, colonna_iniziale + larghezza_quadrato, riga_iniziale + altezza_quadrato);
+      draw_square(2, altezza_quadrato, larghezza_quadrato, colonna_iniziale + larghezza_quadrato, riga_iniziale);
+      draw_square(2, altezza_quadrato, larghezza_quadrato, colonna_iniziale, riga_iniziale + altezza_quadrato);
+    
+      riga_iniziale+= altezza_quadrato * 2;
+      c++;
+    }
+  c = 0;
+  i++;
+  riga_iniziale = temp;
+  colonna_iniziale+= larghezza_quadrato * 2;
+  }  
+  
+  
+  draw_end();
   
   return 0;
 }
