c75c8b275340d37873120c65643be5b4dd370ecf
Teodoro777 <teodoro.coluccio@gmail.com>
2016-12-22 23:46:48 +0100

    Aggiunte funzioni per la stampa di una colonna di quadrati e una scacchiera

--- C/scacchiera/scacchiera.c
+++ C/scacchiera/scacchiera.c
@@ -36,6 +36,33 @@ void draw_square(int colore, int altezza, int larghezza, int colonna_iniziale, i
   attroff(COLOR_PAIR(colore));
 }
 
+void draw_four_square(int altezza_quadrato, int larghezza_quadrato, int colonna_iniziale, int riga_iniziale)
+{
+  int c = 0;
+
+  while (c < 4) {
+    draw_square(1, altezza_quadrato, larghezza_quadrato, colonna_iniziale, riga_iniziale);
+    draw_square(1, altezza_quadrato, larghezza_quadrato, colonna_iniziale + larghezza_quadrato, riga_iniziale + altezza_quadrato);
+    draw_square(2, altezza_quadrato, larghezza_quadrato, colonna_iniziale + larghezza_quadrato, riga_iniziale);
+    draw_square(2, altezza_quadrato, larghezza_quadrato, colonna_iniziale, riga_iniziale + altezza_quadrato);
+    
+    riga_iniziale+= altezza_quadrato * 2;
+    c++;
+  }
+}
+
+void draw_chessboard(int altezza_quadrato, int larghezza_quadrato, int colonna_iniziale, int riga_iniziale)
+{
+  int i = 0, temp = riga_iniziale;
+
+  while (i < 4) {
+    draw_four_square(altezza_quadrato, larghezza_quadrato, colonna_iniziale, riga_iniziale);
+    riga_iniziale = temp;
+    colonna_iniziale+= larghezza_quadrato * 2;
+    i++;
+  }
+}
+
 void draw_end()
 { 
   refresh();
--- C/scacchiera/scacchiera.h
+++ C/scacchiera/scacchiera.h
@@ -3,6 +3,8 @@
 
 void draw_init();
 void draw_square(int colore, int altezza, int larghezza, int colonna_iniziale, int riga_iniziale);
+void draw_four_square(int altezza_quadrato, int larghezza_quadrato, int colonna_iniziale, int riga_iniziale);
+void draw_chessboard(int altezza_quadrato, int larghezza_quadrato, int colonna_iniziale, int riga_iniziale);
 void draw_end();
 
 #endif
