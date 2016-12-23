6f4aaef7f018fae75ee4f4bdc244098a109c37ad
Fabio Z Tessitore <fabioztessitore@gmail.com>
2016-12-23 09:38:42 +0100

    controllo four square

--- C/scacchiera/main.c
+++ C/scacchiera/main.c
@@ -2,14 +2,12 @@
 
 /* disegno di una scacchiera con le ncurses */
 
-#include <stdlib.h>
-
 #include "scacchiera.h"
 
 int main()
 {
   int riga_iniziale = 1, colonna_iniziale = 9, altezza_quadrato = 3, larghezza_quadrato = 4;
-  
+
   /* Nella funzione ho dichiarato le coppie di colore:
    * - 1 = COLOR_BLACK, COLOR_WHITE
    * - 2 = COLOR_BLACK, COLOR_RED
@@ -18,10 +16,11 @@ int main()
    */
 
   draw_init();
-  
-  draw_chessboard(altezza_quadrato, larghezza_quadrato, colonna_iniziale, riga_iniziale);
-  
+
+  /*draw_chessboard(altezza_quadrato, larghezza_quadrato, colonna_iniziale, riga_iniziale);*/
+  draw_four_square(altezza_quadrato, larghezza_quadrato, colonna_iniziale, riga_iniziale);
+
   draw_end();
-  
+
   return 0;
 }
