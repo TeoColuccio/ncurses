70fceb9c72b109448c644ce4c3d5070f2d437985
Teodoro777 <teodoro.coluccio@gmail.com>
2017-01-08 23:48:32 +0100

    Aggiunto modulo mad_editor in python

---
+++ Python/mad_editor/mad_editor.py
@@ -0,0 +1,38 @@
+import random
+import curses
+
+def mad_init():
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.curs_set(0)
+    curses.keypad(stdscr, TRUE)
+
+def mad_stampa_intestazione():
+    stdscr.addstr(4, 4, "Questo e' un editor di testi matto\n    (Premi F2 per terminare)")
+
+def mad_stampa_carattere(c):
+    (rows, cols) = stdscr.getmaxyx()
+    attr = random.random_between(0, 3)
+  
+    if attr == 0:
+        stdscr.attron(A_UNDERLINE)
+        stdscr.mvprintw(random.randrange(0, rows), random.randrange(0, cols), "%c", c)
+        stdscr.attroff(A_UNDERLINE)
+    elif attr ==  1:
+        stdscr.attron(A_BOLD)
+        stdscr.mvprintw(random_between(0, rows), random_between(0, cols), "%c", c)
+        stdscr.attroff(A_BOLD)
+    elif attr == 2:
+        stdscr.attron(A_STANDOUT)
+        stdscr.mvprintw(random_between(0, rows), random_between(0, cols), "%c", c)
+        stdscr.attroff(A_STANDOUT)
+
+def mad_stampa_carattere_ciclo():
+    while car == stdscr.getch() != KEY_F(2) 
+        c = stdscr.getch()
+        mad_stampa_carattere(c)
+    
+def mad_end():
+    stdscr.refresh
+    stdscr.getch()
+    stdscr.endwin()
---
+++ Python/mad_editor/main.py
@@ -0,0 +1,17 @@
+# Python  
+#   Mad editor 
+
+# Il programma legge un carattere da tastiera 
+# e lo stampa in una posizione casuale dello schermo con
+# un attributo casuale. Premere f2 per uscire 
+
+from mad_editor import *
+  
+  mad_init()
+
+  mad_stampa_intestazione()
+
+  mad_stampa_carattere_ciclo()
+ 
+  mad_end()
+
